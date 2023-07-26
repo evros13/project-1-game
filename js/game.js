67c4ef3d991fe1ede316c543eac8e6c4de8f879a
Edna Vidal <ednavidalros@gmail.com>
2023-07-26 13:33:25 +0200

    Life counter working and styled.

--- js/game.js
+++ js/game.js
@@ -6,49 +6,40 @@ class Game {
         this.player = new Player(this.gameScreen);
         this.obstaclesArray = [];
         this.rewardsArray = [];
+        this.obstaclesZeroLivesArray = []
 
         this.gameIsOver = false;
         this.score = 0; // Starting score will always be 0
-        this.lives = 1; // Starting number of lives will always be 5
+        this.lives = 5; // Starting number of lives will always be 5
     }
 
 
     checkGameIsOver() {
         if (this.lives === 0) {
             this.gameIsOver = true;
- 
+
             this.gameScreen.style.display = "none";
             this.gameOverScreen.style.display = "block";
 
         }
     }
-    
 
-    // updateLifeCounter() {
-    // const lifeCounter = document.getElementById("livescounter")
-    // const liveImg = document.getElementsByClassName("lives")
 
-    //     liveImg.forEach((life, index) => {
-    //         if (index < lifeCounter) {
-    //             liveImg.classList.remove("hidden");
-    //         } else {
-    //             heart.classList.add("hidden");
-    //         }
-    //     });
-    // }
+    updateLifeCounter() {
+        const livesCounter = document.getElementById("livesCounter")
+        livesCounter.innerHTML = "";
+        for (let i = 0; i < this.lives; i++) {
+            const lifeImg = document.createElement("div");
+            livesCounter.appendChild(lifeImg);
+            lifeImg.className = "lives"
+        }
+
+
+        // We want to check how many lives we have this.lives
+
+        // we are going to render inside livescounter as many divs as lives
+
+
     }
+}
 
-
-
-
-
-
-    // Start the game
-    // start() {
-    //     // Hide the start screen and display the game screen
-    //     this.startScreen.style.display = "none";
-    //     this.gameScreen.style.display = "block";
-
-    //     console.log("testing start")
-    //     this.gameLoop();
-    // }
--- js/script.js
+++ js/script.js
@@ -11,8 +11,10 @@ window.onload = function () {
         gameLoop(); // Use setTimeout and setInterval to create and move obstacles and rewards at regular intervals
         setTimeout(() => setInterval(createObstacle, 3000), 350);
         setTimeout(() => setInterval(createReward, 5000), 10000);
+        setTimeout(() => setInterval(createObstacleZeroLives, 7000), 15000);
         setInterval(moveObstacle, 20);
         setInterval(moveReward, 20);
+        setInterval(moveObstacleZeroLives, 20);
         scoreCounterFunction(newGame); // Call the scoreCounterFunction to update the game score
     }
 
@@ -22,33 +24,37 @@ window.onload = function () {
     startButton.addEventListener("click", function () { // Game will start only after pressing the startButton
         startGame()
         startButton.style.display = "none";
-        console.log("CLICKING")
-
+        console.log("Starting Game...")
+        
     })
-
+    
     const restartButton = document.getElementById("restartButton")
     restartButton.addEventListener("click", function () { // Game will restart
         window.location.reload();
-        console.log("RESTART BUTTON")
-
+        console.log("Restarting game...")
+        
     })
-
-
+    
+    
     document.addEventListener("keydown", function (event) { // This eventListener links the keyboard to the code
         newPlayer.playerMoves(event.key);
     });
-
-
+    
+    
     function gameLoop() { // Function used to run the game loop
         frames++;
+        
+        newGame.updateLifeCounter();
+        
+        hitByObstacle(); // Check for collisions player-obstacle
 
-            hitByObstacle(); // Check for collisions player-obstacle
+        hitByObstacleZeroLives();
 
-            hitByReward(); // Check for collisions player-reward
+        hitByReward(); // Check for collisions player-reward
 
-            requestAnimationFrame(gameLoop); // Request the next animation frame to continue the game loop
+        requestAnimationFrame(gameLoop); // Request the next animation frame to continue the game loop
 
-            newGame.checkGameIsOver();
+        newGame.checkGameIsOver();
 
     }
 
@@ -89,31 +95,7 @@ window.onload = function () {
     }
 
 
-    // Functions to create and move rewards
-
-    function createReward() {
-        const reward = document.createElement("div");
-        gameScreen.appendChild(reward);
-        reward.className = "reward";
-        reward.style.top = 0;
-        reward.style.left = (Math.random() * newGame.gameScreen.offsetWidth) + "px";
-        newGame.rewardsArray.push(reward);
-    }
-
-
-    function moveReward() {
-        for (let i = 0; i < newGame.rewardsArray.length; i++) {
-            let currentPositionReward = parseInt(newGame.rewardsArray[i].style.top);
-            newGame.rewardsArray[i].style.top = (currentPositionReward + 1) + "px";
-
-            if (newGame.rewardsArray[i].offsetTop >= 495) {
-                newGame.rewardsArray[i].remove();
-                newGame.rewardsArray.splice(i, 1);
-            }
-        }
-    }
-
-    // Check for collisions between the player and obstacles
+    // Functions to create and move obstacles and check if player collides with them
     function hitByObstacle() {
         newGame.obstaclesArray.forEach((obstacle) => {
             const playerPosition = newPlayer.playerElement.getBoundingClientRect();
@@ -131,19 +113,41 @@ window.onload = function () {
 
             }
 
-            if(newGame.lives === 0) {
+            if (newGame.lives === 0) {
                 newGame.gameIsOver = true;
             }
 
-            
+
 
         });
 
 
     }
 
+    // Functions to create and move rewards and check if player collides with them
 
-    // Check player has collided with reward
+    function createReward() {
+        const reward = document.createElement("div");
+        gameScreen.appendChild(reward);
+        reward.className = "reward";
+        reward.style.top = 0;
+        reward.style.left = (Math.random() * newGame.gameScreen.offsetWidth) + "px";
+        newGame.rewardsArray.push(reward);
+    }
+
+    function moveReward() {
+        for (let i = 0; i < newGame.rewardsArray.length; i++) {
+            let currentPositionReward = parseInt(newGame.rewardsArray[i].style.top);
+            newGame.rewardsArray[i].style.top = (currentPositionReward + 1) + "px";
+
+            if (newGame.rewardsArray[i].offsetTop >= 495) {
+                newGame.rewardsArray[i].remove();
+                newGame.rewardsArray.splice(i, 1);
+            }
+        }
+    }
+
+
     function hitByReward() {
         newGame.rewardsArray.forEach((reward) => {
             const playerPosition = newPlayer.playerElement.getBoundingClientRect();
@@ -164,4 +168,51 @@ window.onload = function () {
         });
 
     }
+
+// Functions to create, move and check if player is hit by obstacles that take away all lives with one collision 
+
+    function createObstacleZeroLives() {
+        const obstacleZeroLives = document.createElement("div");
+        gameScreen.appendChild(obstacleZeroLives);
+        obstacleZeroLives.className = "obstacleZeroLives";
+        obstacleZeroLives.style.top = 0;
+        obstacleZeroLives.style.left = (Math.random() * newGame.gameScreen.offsetWidth) + "px";
+        newGame.obstaclesZeroLivesArray.push(obstacleZeroLives);
+    }
+
+
+    function moveObstacleZeroLives() {
+        for (let i = 0; i < newGame.obstaclesZeroLivesArray.length; i++) {
+            let currentPositionObstacleZeroLives = parseInt(newGame.obstaclesZeroLivesArray[i].style.top);
+            newGame.obstaclesZeroLivesArray[i].style.top = (currentPositionObstacleZeroLives + 1) + "px";
+
+            if (newGame.obstaclesZeroLivesArray[i].offsetTop >= 495) {
+                newGame.obstaclesZeroLivesArray[i].remove();
+                newGame.obstaclesZeroLivesArray.splice(i, 1);
+            }
+        }
+    }
+
+
+    function hitByObstacleZeroLives() {
+        newGame.obstaclesZeroLivesArray.forEach((obstacleZeroLives) => {
+            const playerPosition = newPlayer.playerElement.getBoundingClientRect();
+            const obstacleZeroLivesPosition = obstacleZeroLives.getBoundingClientRect();
+
+            if (
+                playerPosition.left < obstacleZeroLivesPosition.left + obstacleZeroLivesPosition.width &&
+                playerPosition.left + playerPosition.width > obstacleZeroLivesPosition.left &&
+                playerPosition.top < obstacleZeroLivesPosition.top + obstacleZeroLivesPosition.height &&
+                playerPosition.top + playerPosition.height > obstacleZeroLivesPosition.top
+            ) {
+                obstacleZeroLives.remove();
+                newGame.lives = 0;
+                newGame.gameIsOver = true;
+                console.log("HIT BY OBSTACLE ZERO LIVES")
+
+            }
+
+
+        });
+}
 }
